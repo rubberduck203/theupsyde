6d688913b36fc74d75cb352684d5054d16dd5f18
Christopher J. McClellan <ckuhn203@gmail.com>
2016-11-12 09:02:43 -0500

    Hello world express app

--- api/WebApi/WebApi/server.js
+++ api/WebApi/WebApi/server.js
@@ -1,6 +1,12 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+var app = express();
+
+app.get('/',
+    function(request, response) {
+        response.send('Hello World');
+    });
+
+app.listen(1337,
+    function() {
+        console.log('Web api is listening on port 1337');
+    });
