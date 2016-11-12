c30979208d8f137042b538516d90e12b780ae6df
Christopher J. McClellan <ckuhn203@gmail.com>
2016-11-12 11:06:21 -0500

    Removed all the hardcoding from root request

--- api/WebApi/WebApi/server.js
+++ api/WebApi/WebApi/server.js
@@ -6,29 +6,46 @@ var app = express();
 app.get('/',
     function (request, response) {
 
+        // API root returns info about the api itself
+        var apiInfo = {
+            Title: 'Todo API',
+            _links: [
+                { self: { href: '/' } },
+                {
+                    todo: [
+                        { href: '/todo' },
+                        {
+                            href: '/todo/{id}',
+                            templated: true
+                        }
+                    ]
+                }
+            ]
+        };
+
         response.format({
-            html: function() {
-                response.send('<h3>Todo API</h3><ul>' +
-                    '<li><a href="/todo">/todo</a></li>' +
-                    '<li>/todo/{id}</li></ul>');
+            html: function () {
+                var links = apiInfo._links[1].todo;
+                var linksMarkup = '<ul>';
+
+                for (var i = 0; i < links.length; i++) {
+
+                    linksMarkup = linksMarkup + '<li>';
+                    if (links[i].templated) {
+                        linksMarkup += links[i].href;
+                    } else {
+                        linksMarkup += '<a href="' + links[i].href + '">' + links[i].href + '</a>';
+                    }
+
+                    linksMarkup += '</li>';
+                }
+                linksMarkup += '</ul>';
+
+                response.send('<h3>' + apiInfo.Title + '</h3>' + linksMarkup);
             },
 
-            json: function() {
-                response.send([
-                    {
-                        'Title': 'Todo API',
-                        '_links': {
-                            'self': { 'href': '/' },
-                            'todo': [
-                                { 'href': '/todo' },
-                                {
-                                    'href': '/todo/{id}',
-                                    'templated': true
-                                }
-                            ]
-                        }
-                    }
-                ]);
+            json: function () {
+                response.send(apiInfo);
             }
         });
     });
