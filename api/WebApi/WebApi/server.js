38fa4ea6827d76ed544915e303b9c1f267674ff1
Christopher J. McClellan <ckuhn203@gmail.com>
2016-11-12 10:12:26 -0500

    simpler conditional syntax

--- api/WebApi/WebApi/server.js
+++ api/WebApi/WebApi/server.js
@@ -6,35 +6,31 @@ var app = express();
 app.get('/',
     function (request, response) {
 
-        if (request.accepts('html')) {
-            response.type('html');
-            response.write('<h3>Todo API</h3');
-            response.write('<ul><li>');
-            response.write('<a href="/todo">/todo</a>');
-            response.write('</li><li>');
-            response.write('/todo/{id}');
-            response.write('</li></ul>');
-            response.send();
-            return;
-        }
+        response.format({
+            html: function() {
+                response.send('<h3>Todo API</h3><ul>' +
+                    '<li><a href="/todo">/todo</a></li>' +
+                    '<li>/todo/{id}</li></ul>');
+            },
 
-        if (request.accepts('json')) {
-            response.send([
-                {
-                    'Title': 'Todo API',
-                    '_links': {
-                        'self': { 'href': '/' },
-                        'todo': [{ 'href': '/todo' },
-                            {
-                                'href': '/todo/{id}',
-                                'templated': true
-                            }]
+            json: function() {
+                response.send([
+                    {
+                        'Title': 'Todo API',
+                        '_links': {
+                            'self': { 'href': '/' },
+                            'todo': [
+                                { 'href': '/todo' },
+                                {
+                                    'href': '/todo/{id}',
+                                    'templated': true
+                                }
+                            ]
+                        }
                     }
-                }
-            ]);
-            return;
-        }
-
+                ]);
+            }
+        });
     });
 
 app.get('/todo', todo.findAll);
