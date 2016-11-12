d132871e64e7015ef8d7d68e08465844ecb16c38
Christopher J. McClellan <ckuhn203@gmail.com>
2016-11-12 10:04:30 -0500

    Added route module

---
+++ api/WebApi/WebApi/routes/todo.js
@@ -0,0 +1,7 @@
+﻿exports.findAll = function(request, response) {
+    response.send([{ name: 'Hello' }, { name: 'World' }]);
+}
+
+exports.findById = function(request, response) {
+    response.send({ id: request.params.id, name: 'Hello', description: 'Awesome sauce!' });
+}
--- api/WebApi/WebApi/server.js
+++ api/WebApi/WebApi/server.js
@@ -1,12 +1,46 @@
-﻿var express = require('express');
+﻿var express = require('express'),
+    todo = require('./routes/todo');
+
 var app = express();
 
 app.get('/',
-    function(request, response) {
-        response.send('Hello World');
+    function (request, response) {
+
+        if (request.accepts('html')) {
+            response.type('html');
+            response.write('<h3>Todo API</h3');
+            response.write('<ul><li>');
+            response.write('<a href="/todo">/todo</a>');
+            response.write('</li><li>');
+            response.write('/todo/{id}');
+            response.write('</li></ul>');
+            response.send();
+            return;
+        }
+
+        if (request.accepts('json')) {
+            response.send([
+                {
+                    'Title': 'Todo API',
+                    '_links': {
+                        'self': { 'href': '/' },
+                        'todo': [{ 'href': '/todo' },
+                            {
+                                'href': '/todo/{id}',
+                                'templated': true
+                            }]
+                    }
+                }
+            ]);
+            return;
+        }
+
     });
 
+app.get('/todo', todo.findAll);
+app.get('/todo/:id', todo.findById);
+
 app.listen(1337,
-    function() {
+    function () {
         console.log('Web api is listening on port 1337');
     });
