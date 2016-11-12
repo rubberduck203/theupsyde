59b034bb10607015595a26e4318bc1f3cd030e54
Christopher J. McClellan <ckuhn203@gmail.com>
2016-11-12 11:15:48 -0500

    Put root response in it's own module

---
+++ api/WebApi/WebApi/routes/root.js
@@ -0,0 +1,45 @@
+﻿exports.index = function(request, response) {
+
+    // API root returns info about the api itself
+    var apiInfo = {
+        Title: 'Todo API',
+        _links: [
+            { self: { href: '/' } },
+            {
+                todo: [
+                    { href: '/todo' },
+                    {
+                        href: '/todo/{id}',
+                        templated: true
+                    }
+                ]
+            }
+        ]
+    };
+
+    response.format({
+        html: function() {
+            var links = apiInfo._links[1].todo;
+            var linksMarkup = '<ul>';
+
+            for (var i = 0; i < links.length; i++) {
+
+                linksMarkup = linksMarkup + '<li>';
+                if (links[i].templated) {
+                    linksMarkup += links[i].href;
+                } else {
+                    linksMarkup += '<a href="' + links[i].href + '">' + links[i].href + '</a>';
+                }
+
+                linksMarkup += '</li>';
+            }
+            linksMarkup += '</ul>';
+
+            response.send('<h3>' + apiInfo.Title + '</h3>' + linksMarkup);
+        },
+
+        json: function() {
+            response.send(apiInfo);
+        }
+    });
+}
--- api/WebApi/WebApi/server.js
+++ api/WebApi/WebApi/server.js
@@ -1,54 +1,10 @@
 ﻿var express = require('express'),
-    todo = require('./routes/todo');
+    todo = require('./routes/todo'),
+    root = require('./routes/root');
 
 var app = express();
 
-app.get('/',
-    function (request, response) {
-
-        // API root returns info about the api itself
-        var apiInfo = {
-            Title: 'Todo API',
-            _links: [
-                { self: { href: '/' } },
-                {
-                    todo: [
-                        { href: '/todo' },
-                        {
-                            href: '/todo/{id}',
-                            templated: true
-                        }
-                    ]
-                }
-            ]
-        };
-
-        response.format({
-            html: function () {
-                var links = apiInfo._links[1].todo;
-                var linksMarkup = '<ul>';
-
-                for (var i = 0; i < links.length; i++) {
-
-                    linksMarkup = linksMarkup + '<li>';
-                    if (links[i].templated) {
-                        linksMarkup += links[i].href;
-                    } else {
-                        linksMarkup += '<a href="' + links[i].href + '">' + links[i].href + '</a>';
-                    }
-
-                    linksMarkup += '</li>';
-                }
-                linksMarkup += '</ul>';
-
-                response.send('<h3>' + apiInfo.Title + '</h3>' + linksMarkup);
-            },
-
-            json: function () {
-                response.send(apiInfo);
-            }
-        });
-    });
+app.get('/', root.index);
 
 app.get('/todo', todo.findAll);
 app.get('/todo/:id', todo.findById);
