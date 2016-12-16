dfcc8b60c001db74f7a69df44b14d57985d03f58
Christopher J. McClellan <ckuhn203@gmail.com>
2016-12-16 11:31:33 -0500

    removed obsolete project

--- api/WebApi/WebApi/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/body-parser/index.d.ts" />
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/lokijs/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/proxyquire/index.d.ts" />
-/// <reference path="globals/sinon/index.d.ts" />
